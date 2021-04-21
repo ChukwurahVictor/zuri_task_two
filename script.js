7b1629e00dca36498dd158edad0c96fbd8b909aa
Victor Webster <chukwurahvictor7@gmail.com>
2021-04-21 02:01:10 +0100

    new commit

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const hostname = 'localhost';
-const port = 4000;
 
 const url = "https://jsonplaceholder.typicode.com/posts";
 
-   https.get(url, res => {
-      let data = [];
+https.get(url, res => {
+   let data = [];
+   
+   res.on('data', chunk => {
+      data.push(chunk);
+   });
+
+   res.on('end', () => {
+      const posts = JSON.parse(Buffer.concat(data).toString());
       
-      res.on('data', chunk => {
-         data.push(chunk);
-      });
+      let baseDir = path.join(__dirname, '/result'); //Set directory
+      const post = JSON.stringify(posts); //convert to json
+      
+      //check if directory exists
+      if(!fs.existsSync(baseDir)) {
+         //Create the directory
+         fs.mkdir(baseDir, { recursive: true }, function(err) {
+            if(err) {
+               console.log(err);
+            } else {
+               //Write to result directory
+               fs.writeFile(`${baseDir}/posts.json`, post, function(err) {
+                  if(err) return console.log(err);
+                  console.log(`Posts saved in ${baseDir}/posts.json`);
+               })
+            }
+         });
 
-      res.on('end', () => {
-         const posts = JSON.parse(Buffer.concat(data).toString());
-
-         let baseDir = path.join(__dirname, '/result'); //Set directory
-         const post = JSON.stringify(posts); //convert to json
-         
+      } else {
          //Write to result directory
-         fs.writeFile(`${baseDir}/posts.json`, post, function(err) {
+         fs.writeFile(`${baseDir}/posts.txt`, post, function(err) {
             if(err) return console.log(err);
             console.log(`Posts saved in ${baseDir}/posts.json`);
          })
-
-      });
-   }).on('error', err => {
-     console.log('Error: ', err.message); //Catch errors
+      }
+      
    });
+}).on('error', err => {
+   console.log('Error: ', err.message); //Catch errors
+});
